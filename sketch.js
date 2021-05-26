cce2f2691f3105d6eefc9bd696c4be159a58b4cf
Swapnil <74098375+SwapnilSuman07@users.noreply.github.com>
2021-05-26 15:57:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,7 +65,7 @@ function draw() {
   else if(keyDown(DOWN_ARROW)){
     updateHeight(0,10);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //balloon.scale = balloon.scale + 0.001;
+    balloon.scale = balloon.scale + 0.002;
     //write code to move air balloon in down direction
   } 
 
